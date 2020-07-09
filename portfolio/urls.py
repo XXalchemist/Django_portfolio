e3c9480395bf25fee582d17fb7fbe160bb545d10
Narayan Krishna <nkrishn8@gmail.com>
2020-07-09 13:36:17 +0530

    configure portfolio/urls.py to route views

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from portfolio import views
 
 urlpatterns = [
-    path('',views.home_page, name='home'),
+    #path('',views.home_page, name='home'),
+    path('',views.project_index, name='project_index'),
+    path('<int:pk>/',views.project_detail, name='project_detail'),
 ]
