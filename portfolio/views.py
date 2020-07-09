3b784f6bea00d87237188f07421051cdec167523
Narayan Krishna <nkrishn8@gmail.com>
2020-07-09 13:31:46 +0530

    project-index view created in portfolio/views.py to show project to project_index.html

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
+from portfolio.models import Project
+
 
 # Create your views here.
 
 
-def home_page(request):
-    return render(request,'home.html',{})
+#def home_page(request):
+#    return render(request,'home.html',{})
+
+def project_index(request):
+    projects = Project.objects.all()
+    context = {
+        'projects': projects
+    }
+
+    return render(request, 'project_index.html',context)
