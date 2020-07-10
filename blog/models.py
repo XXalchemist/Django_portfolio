e3783e476f0db1a68cb478086c06e1db8f563d5a
Narayan Krishna <nkrishn8@gmail.com>
2020-07-10 12:31:48 +0530

    Post class in blog/models.py added

--- blog/models.py
+++ blog/models.py
@@ -2,4 +2,10 @@ from django.db import models
 
 class Category(models.Model): #To define category of each blog
     name = models.CharField(max_length = 20)
-    
+
+class Post(models.Model): #To define each blog posts
+    title = models.CharField(max_length = 255)
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add = True)
+    last_modified = models.DateTimeField(auto_now= True)
+    categories = models.ManyToManyField('Category', related_name = 'posts')
