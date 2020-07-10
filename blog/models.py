c2c66d3b735b50e0fa2093a93ec569f58f705efc
Narayan Krishna <nkrishn8@gmail.com>
2020-07-10 12:40:56 +0530

    added comment class in blog/models.py

--- blog/models.py
+++ blog/models.py
@@ -7,5 +7,12 @@ class Post(models.Model): #To define each blog posts
     title = models.CharField(max_length = 255)
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add = True) #whenever instance of class is created
-    last_modified = models.DateTimeField(auto_now = True) #whenever instance of class is saved
-    categories = models.ManyToManyField('Category', related_name = 'posts')
+    last_modified = models.DateTimeField(auto_now = True) #whenever instance of class is saved i.e whenever an edit in post this time is updated
+    categories = models.ManyToManyField('Category', related_name = 'posts') # many category can be assigned to many posts
+
+class Comment(models.Model):
+    author = models.CharField(max_length = 60)
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add = True)
+    post = models.ForeignKey('Post', on_delete = models.CASCADE)
+    
