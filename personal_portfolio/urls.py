0b305f6da9d14f3aa8826e4c5415d5a670c560c3
Narayan Krishna <nkrishn8@gmail.com>
2020-07-10 13:16:01 +0530

    blog/templates added

--- personal_portfolio/urls.py
+++ personal_portfolio/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('projects/',include('portfolio.urls')),
+    path('blog/',include('blog.urls')),
 ]
